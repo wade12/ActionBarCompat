fa22342687f6d259b14637f07f88a7b4a67445d0
wade12 <wadetaylor@engineer.com>
2013-11-25 11:46:58 +0000

    1st commit.

---
+++ ActionBarCompat/src/com/wade12/actionbarcompat/MainActivity.java
@@ -0,0 +1,24 @@
+package com.wade12.actionbarcompat;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getSupportActionBar().setTitle("OptiWifi");
+	} // end method onCreate
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	} // end method onCreateOptionsMenu
+	
+} // end Class MainActivity
